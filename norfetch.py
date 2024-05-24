5c84dc26ced4496fcc7dd8bbf258112f8f4d7098
Åsmund Balkesen <77496597+balkenix@users.noreply.github.com>
2024-05-24 15:31:42 +0100

    norfetch: optimized bad code

--- norfetch.py
+++ norfetch.py
@@ -3,6 +3,8 @@ import os
 import sys
 import socket
 
+first_argument = ""
+
 colours = {
     "red": "\x1b[31;49m",
     "blue": "\x1b[34;49m",
@@ -20,115 +22,25 @@ arguments = [
 ]
 
 
-class SystemInformation:
+class Norfetch:
     def __init__(self) -> None:
         self.hostname = socket.gethostname()
         self.username = os.getlogin()
+        self.os_name = self.get_os()
         self.uptime = self.get_uptime()
-        self.shell = os.environ.get("SHELL", "some shell idk")
-        self.kernel = os.uname()
+        self.shell = os.environ.get("SHELL", "???")
+        self.kernel = self.get_kernel()
 
         pass
 
-    def print_info(self):
-        arg = ""
-
-        try:
-            arg = sys.argv[1]
-
-        except:
-            print_error()
-
-        if arg not in arguments:
-            print_error()
-
-        if arguments[0] in arg:
-            print("")
-            print(
-                f"{colours['red']}███{colours['white']}██{colours['blue']}██{colours['white']}██{colours['red']}█████████   user:    {self.username}"
-            )
-            print(
-                f"{colours['white']}█████{colours['blue']}██{colours['white']}███████████   host:    {self.hostname}"
-            )
-            print(f"{colours['blue']}██████████████████   up:      {self.uptime}")
-            print(
-                f"{colours['white']}█████{colours['blue']}██{colours['white']}███████████   shell:   {self.shell}"
-            )
-            print(
-                f"{colours['red']}███{colours['white']}██{colours['blue']}██{colours['white']}██{colours['red']}█████████   kernel:  {self.kernel.sysname} {self.kernel.release} {self.kernel.machine}"
-            )
-
-        if arguments[1] in arg:
-            print(
-                f"{colours['blue']}███{colours['white']}██{colours['red']}██{colours['white']}██{colours['blue']}█████████   user:    {self.username}"
-            )
-            print(
-                f"{colours['white']}█████{colours['red']}██{colours['white']}███████████   host:    {self.hostname}"
-            )
-            print(f"{colours['red']}██████████████████   up:      {self.uptime}")
-            print(
-                f"{colours['white']}█████{colours['red']}██{colours['white']}███████████   shell:   {self.shell}"
-            )
-            print(
-                f"{colours['blue']}███{colours['white']}██{colours['red']}██{colours['white']}██{colours['blue']}█████████   kernel:  {self.kernel.sysname} {self.kernel.release} {self.kernel.machine}"
-            )
-        if arguments[2] in arg:
-            print(
-                f"{colours['red']}█████{colours['white']}██{colours['red']}███████████   user:    {self.username}"
-            )
-            print(
-                f"{colours['red']}█████{colours['white']}██{colours['red']}███████████   host:    {self.hostname}"
-            )
-            print(f"{colours['white']}██████████████████   up:      {self.uptime}")
-            print(
-                f"{colours['red']}█████{colours['white']}██{colours['red']}███████████   shell:   {self.shell}"
-            )
-            print(
-                f"{colours['red']}█████{colours['white']}██{colours['red']}███████████   kernel:  {self.kernel.sysname} {self.kernel.release} {self.kernel.machine}"
-            )
-        if arguments[3] in arg:
-            print(
-                f"{colours['white']}█████{colours['blue']}██{colours['white']}███████████   user:    {self.username}"
-            )
-            print(
-                f"{colours['white']}█████{colours['blue']}██{colours['white']}███████████   host:    {self.hostname}"
-            )
-            print(f"{colours['blue']}██████████████████   up:      {self.uptime}")
-            print(
-                f"{colours['white']}█████{colours['blue']}██{colours['white']}███████████   shell:   {self.shell}"
-            )
-            print(
-                f"{colours['white']}█████{colours['blue']}██{colours['white']}███████████   kernel:  {self.kernel.sysname} {self.kernel.release} {self.kernel.machine}"
-            )
-        if arguments[4] in arg:
-            print(
-                f"{colours['blue']}█████{colours['yellow']}██{colours['blue']}███████████   user:    {self.username}"
-            )
-            print(
-                f"{colours['blue']}█████{colours['yellow']}██{colours['blue']}███████████   host:    {self.hostname}"
-            )
-            print(f"{colours['yellow']}██████████████████   up:      {self.uptime}")
-            print(
-                f"{colours['blue']}█████{colours['yellow']}██{colours['blue']}███████████   shell:   {self.shell}"
-            )
-            print(
-                f"{colours['blue']}█████{colours['yellow']}██{colours['blue']}███████████   kernel:  {self.kernel.sysname} {self.kernel.release} {self.kernel.machine}"
-            )
-        if arguments[5] in arg:
-            print(
-                f"{colours['white']}███{colours['blue']}██{colours['red']}██{colours['blue']}██{colours['white']}█████████   user:    {self.username}"
-            )
-            print(
-                f"{colours['blue']}█████{colours['red']}██{colours['blue']}███████████   host:    {self.hostname}"
-            )
-            print(f"{colours['red']}██████████████████   up:      {self.uptime}")
-            print(
-                f"{colours['blue']}█████{colours['red']}██{colours['blue']}███████████   shell:   {self.shell}"
-            )
-            print(
-                f"{colours['white']}███{colours['blue']}██{colours['red']}██{colours['blue']}██{colours['white']}█████████   kernel:  {self.kernel.sysname} {self.kernel.release} {self.kernel.machine}"
-            )
-
+    def print_info(self, colour1, colour2, colour3):
+        white = colours['white']
+        # █ copy me!
+        print(f"{colour1}██{colour2}██{colour3}██{colour2}██{colour1}██████{white}  |  {self.username}@{self.hostname}")
+        print(f"{colour2}████{colour3}██{colour2}████████{white}  |  os:     {self.os_name}")
+        print(f"{colour3}██████████████{white}  |  up:     {self.uptime}")
+        print(f"{colour2}████{colour3}██{colour2}████████{white}  |  shell:  {self.shell}")
+        print(f"{colour1}██{colour2}██{colour3}██{colour2}██{colour1}██████{white}  |  kernel: {self.kernel}")
 
     def get_uptime(self):
         with open("/proc/uptime", "r") as f:
@@ -139,8 +51,22 @@ class SystemInformation:
             "{days:02}:{hours:02}:{minutes:02}:{seconds:02}",
         )
 
+    def get_os(self):
+        with open("/etc/os-release") as f:
+            d = {}
+            for line in f:
+                k,v = line.rstrip().split("=")
+                d[k] = v
 
-def print_error():
+        return d['PRETTY_NAME'].strip('\"')
+
+    def get_kernel(self):
+        kernel = os.uname().sysname
+        version = os.uname().release
+        arch = os.uname().machine
+        return f"{kernel} {version} {arch}"
+
+def print_error_by_no_argument():
     print(
         f"error: {colours['red']}no arguments given when one was needed.{colours['white']}"
     )
@@ -150,6 +76,21 @@ def print_error():
     print("--denmark = danish flag")
     print("--finland = finnish flag")
     print("--sweden  = swedish flag")
+    print("--faroe   = faroese flag")
+
+    sys.exit(-1)
+
+def print_error_by_unknown_argument():
+    print(
+        f"error: {colours['red']}unknown argument{colours['white']}"
+    )
+    print("")
+    print("--norway  = norwegian flag")
+    print("--iceland = icelandic flag")
+    print("--denmark = danish flag")
+    print("--finland = finnish flag")
+    print("--sweden  = swedish flag")
+    print("--faroe   = faroese flag")
 
     sys.exit(-1)
 
@@ -163,7 +104,25 @@ def strfdelta(tdelta, fmt):
     return fmt.format(**d)
 
 
-norfetch = SystemInformation()
+norfetch = Norfetch()
 
-norfetch.print_info()
+try:
+    first_argument = sys.argv[1]
+
+except:
+    print_error_by_no_argument()
+
+if first_argument == arguments[0]:
+    norfetch.print_info(colours['red'], colours['white'], colours['blue'])
+if first_argument == arguments[1]:
+    norfetch.print_info(colours['blue'], colours['white'], colours['red'])
+if first_argument == arguments[2]:
+    norfetch.print_info(colours['red'], colours['red'], colours['white'])
+if first_argument == arguments[3]:
+    norfetch.print_info(colours['white'], colours['white'], colours['blue'])
+if first_argument == arguments[4]:
+    norfetch.print_info(colours['blue'], colours['blue'], colours['yellow'])
+if first_argument == arguments[5]:
+    norfetch.print_info(colours['white'], colours['blue'], colours['red'])
+
 print("")
